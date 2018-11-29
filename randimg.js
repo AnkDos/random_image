61f30fea754fa16aa6372035b38ed702888a3e3d
AnkDos <ankurpan96@gmail.com>
2018-11-29 20:36:43 +0000

    designed UI

--- randimg.js
+++ randimg.js
@@ -1,15 +1,7 @@
 
 function gen_ran(){
-    ran_img = [] ;
-     
     i = 0;
-    while(i<100){
-        rand = Math.round(Math.random()*1000);
-        
-        ran_img.push("https://picsum.photos/800/600/?image="+rand);
-        i+=1;
-    }  
-
-
-    console.log(ran_img);
-}
+    rand = Math.round(Math.random()*1000);
+    document.getElementById('crs').innerHTML = "<img src=https://picsum.photos/800/600/?image="+rand+" >";    
+    } 
+    
